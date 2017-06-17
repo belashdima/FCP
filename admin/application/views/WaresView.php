457aa6699178da6d413da6594bc4cb65c61a77ff
Belash Dima <gbelashdima@gmail.com>
2017-06-18 00:55:49 +0300

    - Navigation in wares - Possibility to view ware

--- admin/application/views/WaresView.php
+++ admin/application/views/WaresView.php
@@ -1,5 +1,5 @@
 <?php
-$balls = $data;?>
+$wares = $data;?>
 
 <!--Add Card-->
 <div class="col-lg-12 col-md-12 col-sm-12 mb-4">
@@ -15,24 +15,24 @@ $balls = $data;?>
 
             <?php
             //print_r($balls);
-            if ($balls != null)
-                foreach ($balls as $ball) {?>
+            if ($wares != null)
+                foreach ($wares as $ware) {?>
                     <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
                         <div class="card h-100">
-                            <a href="http://localhost/Footballcity_Project/admin/wares/ware?id=<?php echo $ball->getWareId(); ?>">
+                            <a href="http://localhost/Footballcity_Project/admin/wares/ware?id=<?php echo $ware->getWareId(); ?>">
                                 <img class="card-img-top img-fluid imageItem" src="http://localhost/Footballcity_Project/images/adidas_x.jpg" alt="">
                             </a>
                             <div class="card-block">
                                 <h4 class="card-title">
-                                    <a href="http://localhost/Footballcity_Project/admin/wares/ware?id=<?php echo $ball->getWareId(); ?>">
-                                        <?php echo $ball->getPropertyValueByName('Brand').' '.$ball->getPropertyValueByName('Model');?>
+                                    <a href="http://localhost/Footballcity_Project/admin/wares/ware?id=<?php echo $ware->getWareId(); ?>">
+                                        <?php echo $ware->getPropertyValueByName('Brand').' '.$ware->getPropertyValueByName('Model');?>
                                     </a>
                                 </h4>
                                 <h5>
-                                    <?php echo $ball->getPropertyValueByName('Price').'$';?>
+                                    <?php echo $ware->getPropertyValueByName('Price').'$';?>
                                 </h5>
                                 <p class="card-text">
-                                    <?php echo $ball->getPropertyValueByName('Description');?>
+                                    <?php /*echo $ware->getPropertyValueByName('Description');*/?>
                                 </p>
                             </div>
                         </div>
