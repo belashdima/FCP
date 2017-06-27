9f324c41c1ac2e301ec87ee28415039d75d88c37
Belash Dima <gbelashdima@gmail.com>
2017-06-27 23:32:00 +0300

    - Added filters in get request to site

--- application/views/ContentView.php
+++ application/views/ContentView.php
@@ -2,44 +2,9 @@
 <div class="row">
     <div class="col-md-3">
 
-
-
-        <?php
-            foreach ($filters as $filter) {
-                if (!strcmp($filter->getProperty()->getPropertyName(), 'Image') == 0 && !strcmp($filter->getProperty()->getPropertyName(), 'Description') == 0) {?>
-                <h4><?php //echo $filter->getProperty()->getPropertyName(); ?></h4>
-                <div class="list-group-item black dropdown"><?php echo $filter->getProperty()->getPropertyName(); ?></div>
-
-                <ul class="list-group filterItem">
-                <?php
-                foreach ($filter->getPossibleValues() as $possibleValue) { ?>
-                    <li class="list-group-item"><?php echo $possibleValue; ?></li>
-                <?php }
-                ?>
-                </ul>
-
-            <?php }
-            }
+        <?php //print_r($data);
+        include_once 'SFiltersView.php';
         ?>
-
-        <h4><?php pr ?></h4>
-
-        <!--<h4>Brand</h4>
-        <ul class="list-group">
-            <li class="list-group-item">Adidas</li>
-            <li class="list-group-item active">Nike</li>
-            <li class="list-group-item">Puma</li>
-            <li class="list-group-item">Select</li>
-            <li class="list-group-item">Select</li>
-            <li class="list-group-item">Umbro</li>
-        </ul>
-
-        <h4>Size</h4>
-        <ul class="list-group">
-            <li class="list-group-item">1</li>
-            <li class="list-group-item">4</li>
-            <li class="list-group-item">5</li>
-        </ul>-->
 
     </div>
 
