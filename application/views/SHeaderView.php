071cd341640c31b52399ed1ac16ea1c54f4a7e4b
Belash Dima <gbelashdima@gmail.com>
2017-07-19 00:33:45 +0300

    - Site menu is inflated from xml

--- application/views/SHeaderView.php
+++ application/views/SHeaderView.php
@@ -1,5 +1,11 @@
 <link rel="stylesheet" href="http://localhost/Footballcity_Project/css/header.css">
 
+<?php
+$xmlMenuItems = simplexml_load_file('http://localhost/Footballcity_Project/xml/menu.xml');
+
+//print_r($xmlMenuItems);
+?>
+
 <header>
 
     <div id="info_header">
@@ -16,105 +22,18 @@
         <div id="expandMobileButton" class="button"></div>
         <ul class="header-ul">
 
-            <li>
-                <a href='http://localhost/Footballcity_Project/boots'>Обувь</a>
-                <ul>
-                    <li>
-                        <a href='http://localhost/Footballcity_Project/football_boots'>Футбольные бутсы</a>
-                    </li>
-                    <li>
-                        <a href='http://localhost/Footballcity_Project/indoor_boots'>Обувь для зала</a>
-                    </li>
-                    <li>
-                        <a href='http://localhost/Footballcity_Project/outdoor_boots'>Шиповки</a>
-                    </li>
-                </ul>
-            </li>
-
-            <li>
-                <a href='http://localhost/Footballcity_Project/balls'>Мячи</a>
-                <ul>
-                    <li>
-                        <a href='http://localhost/Footballcity_Project/football_balls'>Футбольные мячи</a>
-                    </li>
-                    <li>
-                        <a href='http://localhost/Footballcity_Project/futsal_balls'>Футзальные мячи</a>
-                    </li>
-                </ul>
-            </li>
-
-            <li>
-                <a href='#'>Форма</a>
-                <ul>
-                    <li>
-                        <a href='http://localhost/Footballcity_Project/game_t-shirts'>Майки</a>
-                    </li>
-                    <li>
-                        <a href='http://localhost/Footballcity_Project/game_shorts'>Шорты</a>
-                    </li>
-                    <li>
-                        <a href='http://localhost/Footballcity_Project/game_socks'>Гетры</a>
-                    </li>
-                </ul>
-            </li>
-
-            <li>
-                <a href='#'>Тренировочная одежда</a>
-                <ul>
-                    <li>
-                        <a href='http://localhost/Footballcity_Project/warm_tops'>Термомайки</a>
-                    </li>
-                    <li>
-                        <a href='http://localhost/Footballcity_Project/warm_pants'>Термоштаны</a>
-                    </li>
-                    <li>
-                        <a href='http://localhost/Footballcity_Project/jackets'>Куртки и ветровки</a>
-                    </li>
-                    <li>
-                        <a href='http://localhost/Footballcity_Project/socks'>Носки</a>
-                    </li>
-                    <li>
-                        <a href='http://localhost/Footballcity_Project/caps_scarfs'>Шарфы и шапки</a>
-                    </li>
-                    <li>
-                        <a href='http://localhost/Footballcity_Project/gloves'>Перчатки</a>
-                    </li>
-                </ul>
-            </li>
-
-            <li>
-                <a href='http://localhost/Footballcity_Project/bags'>Сумки и рюкзаки</a>
-            </li>
-
-            <li>
-                <a href='#'>Вратарская экипировка</a>
-                <ul>
-                    <li>
-                        <a href='http://localhost/Footballcity_Project/goalkeeper_gloves'>Перчатки</a>
-                    </li>
-                    <li>
-                        <a href='http://localhost/Footballcity_Project/goalkeeper_jackets'>Свитера</a>
-                    </li>
-                    <li>
-                        <a href='http://localhost/Footballcity_Project/goalkeeper_t-shirts'>Майки</a>
-                    </li>
-                    <li>
-                        <a href='http://localhost/Footballcity_Project/goalkeeper_shorts'>Шорты</a>
-                    </li>
-                </ul>
-            </li>
-
-            <li>
-                <a href='#'>Прочее</a>
-                <ul>
-                    <li>
-                        <a href='http://localhost/Footballcity_Project/shin_pads'>Щитки</a>
-                    </li>
-                    <li>
-                        <a href='http://localhost/Footballcity_Project/pumps'>Насосы</a>
-                    </li>
-                </ul>
-            </li>
+            <?php foreach ($xmlMenuItems as $xmlMenuItem) { ?>
+                <li>
+                    <a href='http://localhost/Footballcity_Project/<?php echo $xmlMenuItem->relativeUrl; ?>'><?php echo $xmlMenuItem->rus; ?></a>
+                    <ul>
+                        <?php foreach ($xmlMenuItem->menuSubItems->children() as $xmlMenuSubItem) { ?>
+                            <li>
+                                <a href='http://localhost/Footballcity_Project/<?php echo $xmlMenuSubItem->relativeUrl; ?>'><?php echo $xmlMenuSubItem->rus; ?></a>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </li>
+            <?php } ?>
         </ul>
     </nav>
 </header>
